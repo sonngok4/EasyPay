43db61ae492d3b8a95937cddc90c20076f665389
sonngok4 <sonngo2k40501@gmail.com>
2024-04-18 08:04:17 +0700

    check requirements

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
 # !/bin/bash
 
-python3.9 -m pip install -r requirements.txt
+# active venv
+python3.9 -m venv .venv
+source .venv/bin/activate
+
+# install packages
+pip install wheel
+pip install --upgrade pip
+pip install -r requirements.txt
+
 python3.9 manage.py collectstatic
