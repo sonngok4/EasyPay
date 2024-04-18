2ecfdc9435d3cddd76abe1fbc8e0e8f1e226bcb2
sonngok4 <sonngo2k40501@gmail.com>
2024-04-18 07:34:36 +0700

    check build

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,9 @@
 
 # Build static files
 python3.9 -m venv venv
-source .env\Scripts\activate
+source .venv\Scripts\activate
 pip install --upgrade pip && \
     pip install wheel && \
     pip install -r requirements.txt
 # python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate --noinput
